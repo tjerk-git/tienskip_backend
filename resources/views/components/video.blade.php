b324fd74e1318dc247ed453943b919c14a6d787c
Tjerk Dijkstra <tjerk.dijkstra@icloud.com>
2024-05-06 15:13:43 +0200

    fix the video

--- resources/views/components/video.blade.php
+++ resources/views/components/video.blade.php
@@ -1,3 +1,3 @@
 <video class="video radius-1" autoplay muted loop>
-      <source src="https://www.dropbox.com/scl/fi/ublsy0h8w5ngujon1nq6p/tskipper.mp4?rlkey=ymg8t2vjqhc3mbzwk3u76mzxg&raw=1" type="video/mp4">
+      <source src="{{ asset('images/video/tskipper.mp4') }}" type="video/mp4">
 </video>
