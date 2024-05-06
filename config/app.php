9716211f4fec5997c73df7f558bfac22386087a7
Tjerk Dijkstra <tjerk.dijkstra@icloud.com>
2024-05-06 17:05:36 +0200

    ok

--- config/app.php
+++ config/app.php
@@ -80,7 +80,7 @@ return [
 
     'locale' => env('APP_LOCALE', 'nl'),
 
-    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
+    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'nl'),
 
     'faker_locale' => env('APP_FAKER_LOCALE', 'en_US'),
 
