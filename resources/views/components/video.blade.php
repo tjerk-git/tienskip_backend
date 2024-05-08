51bdc58c1c2b655c8492a83a3563b06658a08020
Tjerk Dijkstra <tjerk.dijkstra@icloud.com>
2024-05-08 14:05:35 +0200

    video play

--- resources/views/components/video.blade.php
+++ resources/views/components/video.blade.php
@@ -1,3 +1,3 @@
-<video class="video radius-1" autoplay muted loop>
-      <source src="{{ asset('images/video/tskipper.mp4') }}" type="video/mp4">
+<video class="video radius-1"  autoplay="autoplay" loop="loop" muted defaultMuted playsinline oncontextmenu="return false;" preload="auto">
+      <source src="{{ asset('images/video/tskipper.mp4') }}" type="video/mp4" >
 </video>
